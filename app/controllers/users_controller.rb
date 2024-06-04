3007320a73631fb1da546c7942db843191d19e97
EC2 Default User <ec2-user@ip-172-31-0-205.ap-northeast-1.compute.internal>
2024-06-04 09:39:36 +0000

    devise設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,21 @@
 class UsersController < ApplicationController
+  
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
   def show
-    @user = User.find(params[:id])
-    @post_images = @user.post_images.page(params[:page])
+  @user = User.find(params[:id])
+  @post_images = @user.post_images.page(params[:page])
   end
 
   def edit
-    @user = User.find(params[:id])
+  @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(usre.id)
+  @user = User.find(params[:id])
+  @user.update(user_params)
+  redirect_to user_path(@user.id)
   end
-
 
   private
 
