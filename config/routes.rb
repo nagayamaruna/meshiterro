a574f3ac828ff7a377fa36b83d627c61ec7c3e12
EC2 Default User <mooncat.rrr@gmail.com>
2024-06-28 11:11:39 +0000

    構成の改善（2）

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
+
   devise_for :users
   root to: 'homes#top'
   get 'homes/about', to: 'homes#about', as: :about
@@ -8,16 +12,12 @@ Rails.application.routes.draw do
   get 'homes', to: 'homes#top'
   end
   resources :users, only: [:show, :edit, :update]
-  
-  devise_for :admin, skip: [:registrations, :password], controllers: {
-    sessions: 'admin/sessions'
-  }
- 
+
   namespace :admin do
     get 'dashboards', to: 'dashboards#index'
     resources :users, only: [:destroy]
   end
-  
+
   scope module: :public do
     devise_for :users
     root to: 'homes#top'
