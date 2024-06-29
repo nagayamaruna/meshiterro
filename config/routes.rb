5c9396502a8b0ecad9ef8bde22b97dad177aabf6
EC2 Default User <mooncat.rrr@gmail.com>
2024-06-29 11:58:12 +0000

    devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -18,16 +18,16 @@ Rails.application.routes.draw do
     resources :users, only: [:destroy]
   end
 
-  scope module: :public do
+   scope module: :public do
     devise_for :users
     root to: 'homes#top'
     get 'homes/about', to: 'homes#about', as: :about
     resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-      resource :favorites, only: [:create, :destroy]
-      resources :post_comments, only: [:create, :destroy]
-    end
-    resources :users, only: [:show, :edit, :update]
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
   end
+  resources :users, only: [:show, :edit, :update]
+ end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
